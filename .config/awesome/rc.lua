cba49aa9b3edb7a83ec500db0d3fb30eac9e5c15
EXtremeExploit <pedro.montes.alcalde@gmail.com>
2022-08-23 18:17:49 -0300

    Clock marging to battery

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -94,7 +94,7 @@ menubar.utils.terminal = terminal -- Set the terminal for applications that requ
 
 -- {{{ Wibar
 -- Create a textclock widget
-local mytextclock = wibox.widget.textclock("%A-%B (UTC-3) / %Y-%m-%d %T", 1)
+local mytextclock = wibox.widget.textclock(" %A-%B (UTC-3) / %Y-%m-%d %T", 1)
 mytextclock:connect_signal("button::press", function(_, _, _, button)
     if button == 1 then cw.toggle() end
 end)
