8df1dcb1315584f7c0faf370f89780fe188166b4
EXtremeExploit <pedro.montes.alcalde@gmail.com>
2024-08-26 12:50:48 -0300

    remove grub-update

--- .config/fish/functions/grub-update.fish
+++
@@ -1,3 +0,0 @@
-function grub-update --wraps='sudo grub-mkconfig -o /boot/grub/grub.cfg' --description 'alias grub-update=sudo grub-mkconfig -o /boot/grub/grub.cfg'
-  sudo grub-mkconfig -o /boot/grub/grub.cfg $argv; 
-end
