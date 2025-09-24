8d7ee26667b8f92459c8deaa5ab39969ab510af6
EXtremeExploit <pedro.montes.alcalde@gmail.com>
2025-09-24 17:32:40 -0300

    awesome/utils: Add utils file

---
+++ .config/awesome/utils.lua
@@ -0,0 +1,32 @@
+local function isModuleAvailable(name)
+    if package.loaded[name] then
+        return true
+    else
+        for _, searcher in ipairs(package.searchers or package.loaders) do
+            local loader = searcher(name)
+            if type(loader) == 'function' then
+                package.preload[name] = loader
+                return true
+            end
+        end
+        return false
+    end
+end
+
+local function dump(o)
+    if type(o) == 'table' then
+        local s = '{ '
+        for k, v in pairs(o) do
+            if type(k) ~= 'number' then k = '"' .. k .. '"' end
+            s = s .. '[' .. k .. '] = ' .. dump(v) .. ','
+        end
+        return s .. '} '
+    else
+        return tostring(o)
+    end
+end
+
+return {
+    isModuleAvailable = isModuleAvailable,
+    dump = dump,
+}
